a78814f600c65f2f3896d9139d0ce918b3b30e89
MauroSantander <mauro.santander94@gmail.com>
2018-05-14 18:18:10 -0300

    Ejercicios clase 7

--- clase7.sql
+++ clase7.sql
@@ -1,6 +1,6 @@
 --Clase 7
 
---1)
+-- ** punto 1 **
 SELECT c.customer_num, fname, lname,
 SUM(total_price*quantity)'Total del Cliente',
 COUNT(DISTINCT i.order_num)'OCs del Cliente',
@@ -15,7 +15,7 @@ FROM items i3)
 AND COUNT(DISTINCT i.order_num) >=2
 GO
 
-SELECT * FROM orders WHERE customer_num=101
+/*SELECT * FROM orders WHERE customer_num=101
 
 SELECT customer_num, COUNT(order_num) FROM orders GROUP BY customer_num
 
@@ -29,9 +29,12 @@ GO
 
 SELECT customer_num, SUM(total_price * quantity)/COUNT(o.order_num) promedio_general FROM items i JOIN orders o ON (i.order_num=o.order_date)
 GROUP BY customer_num
-GO
+GO */
 
---2)
+
+
+-- ** punto 2 **
+
 SELECT s.stock_num, m.manu_code, description, manu_name, SUM(quantity*total_price) 'u$ por producto', SUM(quantity) 'Unidad por producto' 
 INTO #ABC_Productos 
 
@@ -42,6 +45,7 @@ WHERE m.manu_code IN (SELECT s2.manu_code
 						GROUP BY s2.manu_code
 						HAVING COUNT(*) >= 10)
 GROUP BY s.stock_num, m.manu_code, description, manu_name
+GO
 
 /*
 FROM (SELECT m.manu_code, COUNT(stock_num) cant, manu_name FROM manufact m JOIN products p ON (m.manu_code=p.manu_code) 
@@ -51,12 +55,13 @@ JOIN products p ON (m.manu_code=p.manu_code)
 JOIN product_types t ON (p.stock_num=t.stock_num)
 JOIN items i ON (p.stock_num=i.stock_num)
 GROUP BY p.stock_num, description, manu_name, m.manu_code
-ORDER BY 5*/
-GO
+ORDER BY 5
+GO*/
 
 
 
 
+-- ** punto 3 **
 DROP TABLE #ABC_Productos
 
 SELECT m.manu_code, COUNT(stock_num) cant, manu_name FROM manufact m JOIN products p ON (m.manu_code=p.manu_code) 
@@ -71,7 +76,7 @@ GO
 SELECT MONTH(), lanme + ', ' + fname nombre_completo, COUNT(DISTINCT order_num), 'Cant OC por mes' FROM #ABC_Productos
 --hecho
 
---4)
+-- ** punto 4 **
 
 SELECT DISTINCT i1.stock_num, i1.manu_code, c1.customer_num, c1.lname + ', ' + c1.fname Nombre_Cliente1, c2.customer_num,
 c2.lname + ', ' + c2.fname Nombre_Cliente1
@@ -97,45 +102,7 @@ AND i12.manu_code=i2.manu_code
 AND o12.customer_num = c2.customer_num)
 GO
 
---5)
-SELECT TOP 1 COUNT(order_num) cant_ordenes_x_cliente 
-FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
-GROUP BY c.customer_num
-ORDER BY 1 DESC
-GO
-
-SELECT TOP 1 SUM(total_price*quantity) lalala
-FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
-JOIN items i ON (i.order_num=o.order_num)
-GROUP BY c.customer_num
-ORDER BY 1 DESC
-GO
-
-SELECT TOP 1 COUNT(item_num) FROM orders o JOIN customer c ON (o.customer_num = c.customer_num) 
-JOIN items i ON (o.order_num = i.order_num)
-GROUP BY c.customer_num, o.order_num
-ORDER BY 1 DESC
-GO
-
-SELECT TOP 1 COUNT(order_num) cant_ordenes_x_cliente 
-FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
-GROUP BY c.customer_num
-ORDER BY 1
-GO
-
-SELECT TOP 1 SUM(total_price*quantity) lalala
-FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
-JOIN items i ON (i.order_num=o.order_num)
-GROUP BY c.customer_num
-ORDER BY 1
-GO
-
-SELECT TOP 1 COUNT(item_num) FROM orders o JOIN customer c ON (o.customer_num = c.customer_num) 
-JOIN items i ON (o.order_num = i.order_num)
-GROUP BY c.customer_num, o.order_num
-ORDER BY 1
-GO
-
+-- ** punto 5 **
 SELECT (SELECT TOP 1 COUNT(order_num) cant_ordenes_x_cliente 
 FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
 GROUP BY c.customer_num
@@ -164,8 +131,47 @@ GROUP BY c.customer_num, o.order_num
 ORDER BY 1) menor_cant_items_en_una_oc
 GO
 
+/*SELECT TOP 1 COUNT(order_num) cant_ordenes_x_cliente 
+FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
+GROUP BY c.customer_num
+ORDER BY 1 DESC
+GO
 
---6)
+SELECT TOP 1 SUM(total_price*quantity) lalala
+FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
+JOIN items i ON (i.order_num=o.order_num)
+GROUP BY c.customer_num
+ORDER BY 1 DESC
+GO
+
+SELECT TOP 1 COUNT(item_num) FROM orders o JOIN customer c ON (o.customer_num = c.customer_num) 
+JOIN items i ON (o.order_num = i.order_num)
+GROUP BY c.customer_num, o.order_num
+ORDER BY 1 DESC
+GO
+
+SELECT TOP 1 COUNT(order_num) cant_ordenes_x_cliente 
+FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
+GROUP BY c.customer_num
+ORDER BY 1
+GO
+
+SELECT TOP 1 SUM(total_price*quantity) lalala
+FROM orders o JOIN customer c ON (c.customer_num=o.customer_num)
+JOIN items i ON (i.order_num=o.order_num)
+GROUP BY c.customer_num
+ORDER BY 1
+GO
+
+SELECT TOP 1 COUNT(item_num) FROM orders o JOIN customer c ON (o.customer_num = c.customer_num) 
+JOIN items i ON (o.order_num = i.order_num)
+GROUP BY c.customer_num, o.order_num
+ORDER BY 1
+GO
+*/
+
+
+-- ** punto 6 **
 --clientes de california con mas de 4 OC cobradas en 1998
 
 SELECT * INTO #ClientesCalifornia FROM customer WHERE state = 'CA'
@@ -220,18 +226,6 @@ ON (i2.order_num=o.order_num)
 GROUP BY i2.order_num
 GO
 
-
---consutas de prueba
-
---lista los items de la orden 3 y el precio por la cantidad comprada de cada uno de ellos
-SELECT item_num, total_price, quantity, total_price*quantity total FROM items i
-JOIN orders o ON (i.order_num=o.order_num) WHERE o.order_num=1003
-
-SELECT * FROM #ClientesCalifornia
---fin consultas de prueba
-
-SELECT paid_date FROM orders
-
 -- ** punto 7 **
 
 SELECT m.manu_code, manu_name, i.stock_num, description, SUM(quantity) comprasDeOtrosFabricantes
@@ -244,4 +238,44 @@ SELECT m.manu_code, manu_name, i.stock_num, description, SUM(quantity) comprasDe
 		AND i.item_num IN (SELECT item_num FROM items WHERE manu_code != m.manu_code)
 	GROUP BY m.manu_code, manu_name, i.stock_num, description
 	HAVING SUM(quantity) < 10
+GO
+
+-- ** punto 8 **
+SELECT c1.state, sname, (c1.lname + ', ' + c1.fname) apellido_nombre_cliente1, (c2.lname + ', ' + c2.fname) apellido_nombre_cliente2, SUM(ct1.total_dolares) total
+	FROM (SELECT TOP 2 clientes_con_total.state,clientes_con_total.customer_num, clientes_con_total.total_dolares FROM (SELECT c.state, o.customer_num,COUNT(DISTINCT o.order_num)cantidad_de_ordenes, SUM(total_price * quantity) total_dolares
+	FROM orders o JOIN items i ON
+		(o.order_num = i.order_num)
+		JOIN customer c ON (c.customer_num = o.customer_num)
+		WHERE state = 'CA'
+		GROUP BY c.state, o.customer_num) clientes_con_total
+		ORDER BY total_dolares DESC) ct1 JOIN customer c1 ON (ct1.customer_num = c1.customer_num)
+		JOIN ((SELECT TOP 2 clientes_con_total.state, clientes_con_total.customer_num, clientes_con_total.total_dolares FROM (SELECT c.state, o.customer_num,COUNT(DISTINCT o.order_num)cantidad_de_ordenes, SUM(total_price * quantity) total_dolares
+		FROM orders o JOIN items i ON
+		(o.order_num = i.order_num)
+		JOIN customer c ON (c.customer_num = o.customer_num)
+		WHERE state = 'CA'
+		GROUP BY c.state, o.customer_num) clientes_con_total
+		ORDER BY total_dolares DESC)) ct2 ON (ct1.state = ct2.state)
+		JOIN customer c2 ON (ct2.customer_num = c2.customer_num)
+		JOIN state s ON (s.code = c1.state)
+		WHERE c1.customer_num < c2.customer_num
+		GROUP BY c1.state, sname, c1.lname, c1.fname, c2.lname, c2.fname
+GO
+
+-- ** punto 9 **
+SELECT order_num, customer_num, order_date,
+(CASE customer_num
+	WHEN (SELECT TOP 1 mejor_cliente.customer_num FROM (SELECT customer_num, SUM(quantity) total_productos FROM orders o JOIN items i ON (o.order_num = i.order_num)
+		JOIN manufact m ON (i.manu_code = m.manu_code)
+		WHERE m.manu_code = 'ANZ'
+		GROUP BY customer_num) mejor_cliente
+		ORDER BY total_productos DESC) THEN NULL
+	ELSE (order_date + lead_time + 1) END) fecha_modificada
+FROM (SELECT TOP 5 * FROM (SELECT customer_num, o.order_num, order_date, m.manu_code FROM orders o
+JOIN items i ON (o.order_num = i.order_num)
+JOIN manufact m ON (i.manu_code = m.manu_code)
+WHERE m.manu_code = 'ANZ'
+GROUP BY customer_num, order_date, o.order_num, m.manu_code) clientes_anz
+ORDER BY order_date DESC) ca
+JOIN manufact m ON (ca.manu_code = m.manu_code)
 GO
